7409ce895a1d1fb4e4d78e3d0d6092baa4e75761
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-08 20:42:27 -0300

    add task

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,13 +2,11 @@
 
 const fs = require('fs');
 
-const file = process.argv[2]
-
-const string = process.argv[3]
+const file = process.argv[2];
+const string = process.argv[3];
 
 fs.writeFile(file, string, 'utf-8', function (error) {
   if (error) {
-    console.error(error)
-    return
+    console.error(error);
   }
 });
