aea60ed5b4cc1876b48aa73bfee08d0393b8ed35
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-10 20:35:47 -0300

    add task

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const movie_id = process.argv[2]
+const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  if (response.statusCode === 200) {
+    const moviedata = JSON.parse(body).title;
+    console.log(moviedata);
+  }
+});
