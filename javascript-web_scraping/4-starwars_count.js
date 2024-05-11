0a1a1f72f570b419e81f006ca8a247a5884cacbe
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-10 21:26:38 -0300

    fixed bug

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -12,9 +12,9 @@ request(url, function (error, response, body) {
   if (response.statusCode === 200) {
     const films = JSON.parse(body).results;
     let count = 0;
-    for (const film of films) {
+    for (let film of films) {
       if (film.characters.includes(`https://swapi-api.hbtn.io/api/films/${characterid}/`)) {
-        count += 1;
+        count++;
       }
     }
     console.log(count);
