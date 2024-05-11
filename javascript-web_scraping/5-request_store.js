c406e607eae297e5f989ac4eb32a8f4c99569c16
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-10 21:24:55 -0300

    add task

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filepath = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  if (response.statusCode === 200) {
+    fs.writeFile(filepath, body, 'utf-8', function (error) {
+      if (error) {
+        console.error(error);
+      }
+    });
+  }
+});
