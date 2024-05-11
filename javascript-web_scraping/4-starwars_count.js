8ff12a3f32dcc68401c9e1619d5af378910a305d
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-10 21:07:55 -0300

    add task

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+const characterid = 18;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      if (film.characters.includes(`https://swapi-api.hbtn.io/api/films/${characterid}/`)) {
+        count += 1;
+      }
+    }
+    console.log(count);
+  }
+});
