0f38e0ccf8cd31129845658062ad4c9f436b67c3
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-05-11 20:28:32 -0300

    add task

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const todos = JSON.parse(body);
+  const completeTasks = {};
+  todos.forEach(function (todo) {
+    if (todo.completed) {
+      if (completeTasks[todo.userId]) {
+        completeTasks[todo.userId] += 1;
+      } else {
+        completeTasks[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(completeTasks);
+});
